c7e036ca54b43e269c8ae8a10e00a93030471d54
Talita Gabriela Del Poso Vilalva <talitagabriela1216@gmail.com>
2023-05-24 14:17:05 -0400

    Atividade LTP
    24-05-2023

---
+++ indexNew.js
@@ -0,0 +1,29 @@
+class Pessoa {
+    constructor(nome,sobrenome){
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+    }
+
+    mostrar(){
+        try {
+            return this.atributos()
+        } catch(erro){
+            console.log(erro.stack)
+        }
+    }
+
+    atributos(){
+        if(this.nome != "" && this.sobrenome != ""){
+        return this.nome + this.sobrenome
+        } else{
+            throw new Error("Falta nome ou sobrenome.")
+        }
+    }
+}
+
+const pessoa = new Pessoa("","Picolo");
+console.log(pessoa.mostrar())
+
+//name
+//name
+//stack
